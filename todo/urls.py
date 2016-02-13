5866881ac55cab71f4dfba2000381bd7992a9daf
gfroosen <guido.f.roosen@gmail.com>
2016-02-13 11:58:58 +0000

    added index view on homepage

--- todo/urls.py
+++ todo/urls.py
@@ -1,10 +1,14 @@
 from django.conf.urls import patterns, include, url
 
 from django.contrib import admin
+from . import views
+
 admin.autodiscover()
 
 urlpatterns = patterns(
     '',
     url(r'^api/', include('api.urls')),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', views.index, name='index'),
+
 )
